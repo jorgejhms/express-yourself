c2500417c4911f655e9654b9f3e8cf80d354f59c
Jorge Meneses Saco <jorgejhms@gmail.com>
2022-10-04 09:47:58 -0500

    adding delete and animals

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ const {
 const expressions = [];
 seedElements(expressions, 'expressions');
 
+const animals = [];
+seedElements(animals, 'animals');
+
 const PORT = process.env.PORT || 4001;
 
 // Get all expressions
@@ -54,6 +57,64 @@ app.post('/expressions', (req, res, next) => {
         res.status(201).send(receivedExpression);
     } else {
         res.status(400).send();
+    }
+})
+
+// Delete an expression
+app.delete('/expressions/:id', (req, res, next) => {
+    const expressionToDelete = getIndexById(req.params.id, expressions);
+    if (expressionToDelete !== -1) {
+        expressions.splice(expressionToDelete, 1);
+        res.status(204).send();
+    } else {
+        res.status(404).send();
+    }
+})
+
+// Animals
+
+app.get('/animals', (req, res, next) => {
+    res.send(animals);
+})
+
+app.get('/animals/:id', (req, res, next) => {
+    const foundAnimal = getElementById(req.params.id, animals);
+
+    if (foundAnimal) {
+        res.send(foundAnimal);
+    } else {
+        res.status(404).send('Animal not found');
+    }
+})
+
+app.put('/animals/:id', (req, res, next) => {
+    const animalIndex = getIndexById(req.params.id, animals);
+    if (animalIndex !== -1) {
+        updateElement(req.params.id, req.query, animals);
+        res.send(animals[animalIndex]);
+    } else {
+        res.status(404).send();
+    }
+})
+
+app.post('/animals', (req, res, next) => {
+    const receivedAnimal = createElement('animals', req.query);
+
+    if (receivedAnimal) {
+        animals.push(receivedAnimal);
+        res.status(201).send(receivedAnimal);
+    } else {
+        res.status(400).send();
+    }
+})
+
+app.delete('/animals/:id', (req, res, next) => {
+    const animalToDelete = getIndexById(req.params.id, animals);
+    if (animalToDelete !== -1) {
+        animals.splice(animalToDelete, 1);
+        res.status(204).send();
+    } else {
+        res.status(404).send();
     }
 })
 
